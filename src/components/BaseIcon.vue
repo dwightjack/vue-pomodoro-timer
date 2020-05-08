6afce30f5b9b2e0ef2225d1114efc97943cadc9e
dwightjack <marco.solazzi@gmail.com>
2020-05-08 14:01:55 +0900

    icons

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="c-base-icon" aria-hidden="true" :is="icon" />
+</template>
+<script lang="ts">
+import { defineComponent, watch, ref } from '@vue/composition-api';
+import VueTypes from 'vue-types';
+export default defineComponent({
+  setup(props) {
+    const icon = ref<unknown>();
+    watch(
+      () => props.name,
+      async (name) => {
+        if (!name) {
+          icon.value = undefined;
+        }
+        try {
+          icon.value = (await import(`@/assets/zondicons/${name}.svg`)).default;
+          console.log(icon.value);
+        } catch (e) {
+          console.error(e);
+          icon.value = undefined;
+        }
+      },
+    );
+    return { icon };
+  },
+  props: {
+    name: VueTypes.string.isRequired,
+  },
+});
+</script>
+<style scoped>
+.c-base-icon {
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+}
+</style>
--- src/components/TheControls.vue
+++ src/components/TheControls.vue
@@ -4,17 +4,28 @@
     role="group"
     aria-label="Timer controls"
   >
-    <BaseControl :pressed="isPlaying" @click="$emit('play')">Play</BaseControl>
-    <BaseControl :pressed="!isPlaying" @click="$emit('pause')"
-      >Pause</BaseControl
-    >
+    <BaseControl :pressed="isPlaying" @click="$emit('play')">
+      <BaseIcon name="play" />
+      <span class="sr-only">Play</span>
+    </BaseControl>
+    <BaseControl :pressed="!isPlaying" @click="$emit('pause')">
+      <BaseIcon name="pause" />
+      <span class="sr-only">Pause</span>
+    </BaseControl>
     <span class="mx-1 border-l border-blue-400 h-6" role="separator" />
-    <BaseControl @click="$emit('skip')">Skip</BaseControl>
-    <BaseControl @click="$emit('reset')">Reset</BaseControl>
+    <BaseControl @click="$emit('skip')">
+      <BaseIcon name="fast-forward" />
+      <span class="sr-only">Skip</span>
+    </BaseControl>
+    <BaseControl @click="$emit('reset')">
+      <BaseIcon name="reload" />
+      <span class="sr-only">Reset</span>
+    </BaseControl>
   </div>
 </template>
 <script lang="ts">
 import BaseControl from '@/components/BaseControl.vue';
+import BaseIcon from '@/components/BaseIcon.vue';
 import VueTypes from 'vue-types';
 import { defineComponent, computed } from '@vue/composition-api';
 import { Status } from '@/types';
@@ -27,6 +38,7 @@ export default defineComponent({
   },
   components: {
     BaseControl,
+    BaseIcon,
   },
   props: {
     status: VueTypes.oneOf(Object.values(Status)).def(Status.Pause),
