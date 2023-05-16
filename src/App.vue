81d8c0b37dc96b0768c838209d8001bc003a6ee0
dwightjack <marco.solazzi@gmail.com>
2023-05-16 15:25:08 +0900

    refactor and fix

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import TheContainer from '@/components/TheContainer.vue';
 import TheTimerList from '@/components/TheTimerList.vue';
 import LayoutStack from '@/components/LayoutStack.vue';
@@ -64,130 +64,97 @@ import { useLoader } from '@/use/loader';
 import { useAsyncModal } from '@/use/asyncModal';
 import { setupNotifications, createInterval, ID_STORE } from './utils';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    TheContainer,
-    TheTimerList,
-    LayoutStack,
-    TheControls,
-    TheCycleEdit,
-    TheCycle,
-    TheLoader,
-    TheNotificationBar,
-    TheGraphicTimer,
-    BaseToast,
-    BaseButton,
-    TransitionFadeSlide,
+const tickWorker = new Worker('/tick-worker.js');
+
+const { needRefresh, updateServiceWorker } = useRegisterSW();
+
+const { exec, loading } = useLoader();
+const intervalsStore = useStorage<Interval[]>('intervals', [
+  createInterval(IntervalType.Work, 45),
+]);
+const permissionsStore = useStorage<{ notification?: boolean }>(
+  'permissions',
+  {},
+);
+const main = useMain();
+const cycle = useCycle();
+
+const { startTicker, stopTicker } = useTicker(tickWorker, cycle.countDown);
+
+const { notify, askPermission } = useNotification();
+const [notifyBarVisible, notifyBar] = useAsyncModal();
+
+function skip() {
+  stopTicker();
+  cycle.nextInterval();
+  if (main.isPlaying) {
+    startTicker();
+  }
+}
+
+function reset() {
+  main.pause();
+  cycle.resetCycle();
+}
+
+async function saveChanges(newIntervals: Interval[]) {
+  reset();
+  main.editOpen = false;
+  cycle.updateCycle(newIntervals);
+  exec(intervalsStore.save(newIntervals));
+}
+
+function onEditToggle(open: boolean) {
+  main.editOpen = open;
+}
+
+async function initialize() {
+  const storedIntervals = await exec(intervalsStore.load());
+  // we use a function to create unique IDs,
+  // but we need to exclude IDs generate in previous sessions
+  // and stored in local storage
+  ID_STORE.push(...storedIntervals.map(({ id }) => id));
+  cycle.updateCycle(storedIntervals);
+}
+
+async function checkNotifyPermission() {
+  const permissions = await permissionsStore.load();
+  if (permissions.notification !== undefined) {
+    return;
+  }
+  const confirmed = await notifyBar.show();
+  permissionsStore.save({ ...permissions, notification: confirmed });
+
+  if (confirmed) {
+    askPermission();
+  }
+}
+
+const notifyInterval = setupNotifications(notify);
+
+watch(
+  () => main.status,
+  (value, _, onInvalidate) => {
+    onInvalidate(stopTicker);
+    if (value === Status.Play) {
+      startTicker();
+      return;
+    }
+    stopTicker();
   },
-  setup() {
-    const tickWorker = new Worker('/tick-worker.js');
+);
 
-    const { needRefresh, updateServiceWorker } = useRegisterSW();
-
-    const { exec, loading } = useLoader();
-    const intervalsStore = useStorage<Interval[]>('intervals', [
-      createInterval(IntervalType.Work, 45),
-    ]);
-    const permissionsStore = useStorage<{ notification?: boolean }>(
-      'permissions',
-      {},
-    );
-    const main = useMain();
-    const cycle = useCycle();
-
-    const { startTicker, stopTicker } = useTicker(tickWorker, cycle.countDown);
-
-    const { notify, askPermission } = useNotification();
-    const [notifyBarVisible, notifyBar] = useAsyncModal();
-
-    function skip() {
-      stopTicker();
-      cycle.nextInterval();
-      if (main.isPlaying) {
-        startTicker();
-      }
+watch(
+  () => cycle.current,
+  (_, prev) => {
+    if (prev === -1 || !main.isPlaying) {
+      return;
     }
-
-    function reset() {
-      main.pause();
-      cycle.resetCycle();
-    }
-
-    async function saveChanges(newIntervals: Interval[]) {
-      reset();
-      main.editOpen = false;
-      cycle.updateCycle(newIntervals);
-      exec(intervalsStore.save(newIntervals));
-    }
-
-    function onEditToggle(open: boolean) {
-      main.editOpen = open;
-    }
-
-    async function initialize() {
-      const storedIntervals = await exec(intervalsStore.load());
-      // we use a function to create unique IDs,
-      // but we need to exclude IDs generate in previous sessions
-      // and stored in local storage
-      ID_STORE.push(...storedIntervals.map(({ id }) => id));
-      cycle.updateCycle(storedIntervals);
-    }
-
-    async function checkNotifyPermission() {
-      const permissions = await permissionsStore.load();
-      if (permissions.notification !== undefined) {
-        return;
-      }
-      const confirmed = await notifyBar.show();
-      permissionsStore.save({ ...permissions, notification: confirmed });
-
-      if (confirmed) {
-        askPermission();
-      }
-    }
-
-    const notifyInterval = setupNotifications(notify);
-
-    watch(
-      () => main.status,
-      (value, _, onInvalidate) => {
-        onInvalidate(stopTicker);
-        if (value === Status.Play) {
-          startTicker();
-          return;
-        }
-        stopTicker();
-      },
-    );
-
-    watch(
-      () => cycle.current,
-      (_, prev) => {
-        if (prev === -1 || !main.isPlaying) {
-          return;
-        }
-        const { type, duration } = cycle.getCurrent();
-        notifyInterval(type, duration);
-      },
-    );
-
-    onMounted(initialize);
-    onMounted(checkNotifyPermission);
-
-    return {
-      main,
-      cycle,
-      skip,
-      reset,
-      saveChanges,
-      onEditToggle,
-      loading,
-      notifyBarVisible,
-      notifyBar,
-      needRefresh,
-      updateServiceWorker,
-    };
+    const { type, duration } = cycle.getCurrent();
+    notifyInterval(type, duration);
   },
-});
+);
+
+onMounted(initialize);
+onMounted(checkNotifyPermission);
 </script>
