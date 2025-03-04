4701e098307a8550cf1ba36764340696c380c911
dwightjack <marco.solazzi@gmail.com>
2025-03-04 17:03:05 +0900

    remove unused prop

--- src/components/BaseControl.vue
+++ src/components/BaseControl.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { bool, oneOf } from 'vue-types';
+import { oneOf } from 'vue-types';
 defineProps({
-  pressed: bool().def(false),
   type: oneOf(['button', 'reset', 'submit', undefined] as const).def('button'),
 });
 defineSlots<{ default?: () => unknown }>();
@@ -9,12 +8,7 @@ defineSlots<{ default?: () => unknown }>();
 <template>
   <button
     :type="type"
-    class="space-x-1 rounded-sm border-2 border-transparent p-2 leading-none text-blue-500 transition-colors duration-150 dark:text-sky-400"
-    :class="{
-      'bg-blue-200 hover:border-blue-500': pressed,
-      'hover:bg-blue-100 dark:hover:bg-sky-700': !pressed,
-    }"
-    :aria-pressed="pressed"
+    class="space-x-1 rounded-sm border-2 border-transparent p-2 leading-none text-blue-500 transition-colors duration-150 hover:bg-blue-100 dark:text-sky-400 dark:hover:bg-sky-700"
   >
     <slot />
   </button>
