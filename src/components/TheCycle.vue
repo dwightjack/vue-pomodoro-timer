d3ef220939f41589810569ea025fa8f3e0aca8f7
dwightjack <marco.solazzi@gmail.com>
2025-11-17 16:08:13 +0900

    simpler syntax

--- src/components/TheCycle.vue
+++ src/components/TheCycle.vue
@@ -8,22 +8,20 @@ const id = useId();
 const max = computed(() => cycle.intervals.reduce((v, i) => i.duration + v, 0));
 </script>
 <template>
-  <div class="w-full">
+  <div
+    role="group"
+    class="flex w-full items-center justify-center overflow-clip rounded-3xl border-[6px] bg-white"
+    :aria-labelledby="id + '-cycle'"
+  >
     <h2 :id="id + '-cycle'" aria-hidden="true" class="sr-only">Intervals</h2>
-    <div
-      role="group"
-      class="flex w-full items-center justify-center overflow-clip rounded-3xl border-[6px] bg-white"
-      :aria-labelledby="id + '-cycle'"
-    >
-      <IntervalStep
-        v-for="(interval, i) in cycle.intervals"
-        :key="interval.id"
-        :size="(100 * interval.duration) / max"
-        :type="interval.type"
-        :duration="interval.duration"
-        :remaining="cycle.countdowns[i]"
-        :current="i === cycle.current"
-      />
-    </div>
+    <IntervalStep
+      v-for="(interval, i) in cycle.intervals"
+      :key="interval.id"
+      :size="(100 * interval.duration) / max"
+      :type="interval.type"
+      :duration="interval.duration"
+      :remaining="cycle.countdowns[i]"
+      :current="i === cycle.current"
+    />
   </div>
 </template>
